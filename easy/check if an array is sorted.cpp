2d8ddebbfc56de1edc5e52fb0e9dc8a09fd87193
Nidhi1k <88227668+Nidhi1k@users.noreply.github.com>
2022-09-16 10:10:02 +0530

    Update check if an array is sorted.cpp

--- easy/check if an array is sorted.cpp
+++ easy/check if an array is sorted.cpp
@@ -1,19 +1,2 @@
 
-class Solution
-{
-public:
-    int largest(vector<int> &arr, int n)
-    {
-    int max = arr[0];
-    for(int it=0;it<n;it++)
-    {
-        if(arr[it]>max)
-        max=arr[it];
-    }
-    return max;
-     //t->0(n) s->0(1)
-    //  sort(arr.begin(),arr.end());
-    //  return arr[n-1];
-    //t->0(nlogn) s->0(n)
-    }
-};
+ 
